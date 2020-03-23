48d02e3b0ec0816c199b659ab0c79395279fc5a6
Linxerz <pruittj2000@Yahoo.com>
2020-03-23 11:35:47 -0500

    Done.

---
+++ db/migrate/004_create_characters_table.rb
@@ -0,0 +1,8 @@
+class CreateCharactersTable < ActiveRecord::Migration[5.1]
+    def change
+        create_table :characters do |t|
+            t.string :name
+            t.string :catchphrase
+        end
+    end
+end
